600a77c578ae3a01db14598399d94d5d33b8c4e7
codyzu <cody.zuschlag@gmail.com>
2022-07-26 17:11:13 +0200

    working site

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {Container, Row, Col} from 'react-bootstrap';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Container>
+      <Row className="vh-100 overflow-hidden">
+        <Col className="align-self-center">
+          <h1>Not much to see here.</h1>
+          <h1>
+            You probably meant to go to{' '}
+            <a href="https://devrel.codyfactory.eu">devrel.codyfactory.eu</a>
+          </h1>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// eslint-disable-line unicorn/filename-case
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'bootswatch/dist/cyborg/bootstrap.min.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.querySelector('#root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
